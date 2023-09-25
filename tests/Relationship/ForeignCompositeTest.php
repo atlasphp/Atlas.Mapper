561a43f10fcfc69e43501ad386337f7eef5dcdc0
Paul M. Jones <pmjones@pmjones.io>
2023-09-25 08:13:24 -0500

    style touchups

--- tests/Relationship/ForeignCompositeTest.php
+++ tests/Relationship/ForeignCompositeTest.php
@@ -8,58 +8,70 @@ use Atlas\Mapper\Related;
 use Atlas\Pdo\Connection;
 use Atlas\Query\Bind;
 use Atlas\Table\Row;
+use Atlas\Testing\Assertions;
 use PHPUnit\Framework\TestCase;
 
-final class ForeignCompositeTest extends TestCase
+class ForeignCompositeTest extends TestCase
 {
-	public function test()
-	{
-		$foreignStrategy = new ForeignComposite('test', [
-			'native1' => 'foreign1',
-			'native2' => 'foreign2',
-		]);
+    use Assertions;
 
-		$select = new class extends MapperSelect {
-			public function __construct()
-			{
-				parent::__construct(Connection::new('sqlite::memory:'), new Bind());
-			}
-		};
-		$foreignStrategy->modifySelect($select, [
-			new class extends Record {
-				public function __construct()
-				{
-					parent::__construct(
-						new class([
-							'id' => 1,
-							'native1' => 'n1',
-							'native2' => 'n2',
-						]) extends Row {
-							protected $cols = [
-								'id' => null,
-								'native1' => null,
-								'native2' => null,
-							];
-						}, new Related([]));
-				}
-			}
-		]);
+    public function test()
+    {
+        $foreignStrategy = new ForeignComposite('test', [
+            'native1' => 'foreign1',
+            'native2' => 'foreign2',
+        ]);
 
-		$this->assertSame('SELECT
-    
-WHERE
-    ( -- composite keys
-    ("test"."foreign1" = :__1__ AND "test"."foreign2" = :__2__)
-)', $select->getStatement());
-		$this->assertSame([
-			'__1__' => [
-				0 => 'n1',
-				1 => 2,
-			],
-			'__2__' => [
-				0 => 'n2',
-				1 => 2,
-			],
-		], $select->getBindValues());
-	}
+        $select = new class extends MapperSelect {
+            public function __construct()
+            {
+                parent::__construct(Connection::new('sqlite::memory:'), new Bind());
+            }
+        };
+
+        $foreignStrategy->modifySelect($select, [
+            new class extends Record {
+                public function __construct()
+                {
+                    parent::__construct(
+                        new class([
+                            'id' => 1,
+                            'native1' => 'n1',
+                            'native2' => 'n2',
+                        ]) extends Row {
+                            protected $cols = [
+                                'id' => null,
+                                'native1' => null,
+                                'native2' => null,
+                            ];
+                        }, new Related([]));
+                }
+            }
+        ]);
+
+        $this->assertSameSql(
+            'SELECT
+
+            WHERE
+                ( -- composite keys
+                ("test"."foreign1" = :__1__ AND "test"."foreign2" = :__2__)
+            )
+            ',
+            $select->getStatement()
+        );
+
+        $this->assertSame(
+            [
+                '__1__' => [
+                    0 => 'n1',
+                    1 => 2,
+                ],
+                '__2__' => [
+                    0 => 'n2',
+                    1 => 2,
+                ],
+            ],
+            $select->getBindValues()
+        );
+    }
 }
